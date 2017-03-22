10e63c7a22ffedbd65a1ea9d1eece8c333cf5396
hehuan <hehuan_git@126.com>
2017-03-22 11:00:28 +0800

    myfirstprj

---
+++ src/com/qinghuo/git/prj/MyFirstGitPrj.java
@@ -0,0 +1,15 @@
+package com.qinghuo.git.prj;
+/**
+ * 
+ * @author hehuan
+ *
+ */
+public class MyFirstGitPrj {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is my first git prj.");
+		
+	}
+
+}
