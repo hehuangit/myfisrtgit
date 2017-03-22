eb918cab35d0542006db30839b6ef3a6e7a17f67
hehuan <hehuan_git@126.com>
2017-03-22 11:03:46 +0800

    second commit.

--- src/com/qinghuo/git/prj/MyFirstGitPrj.java
+++ src/com/qinghuo/git/prj/MyFirstGitPrj.java
@@ -10,6 +10,10 @@ public class MyFirstGitPrj {
 		
 		System.out.println("This is my first git prj.");
 		
+		for(int i = 0; i < 10; i++) {
+			System.out.println("This is my first git prj.");
+		}
+		
 	}
 
 }
